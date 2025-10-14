24845440fe0a776ceef5632d521c08efa4c44e37
sigmaseven <sigmaseven@gmail.com>
2025-10-14 11:39:39 -0400

    Improve menu icons: sword for weapons, detailed book for army books, detailed clipboard for army lists, detailed upload for import, enhanced shield for factions

--- frontend/src/components/Icons.js
+++ frontend/src/components/Icons.js
@@ -20,15 +20,16 @@ export const Icon = ({ name, size = 20, color = '#8b949e', className = '' }) => 
       </svg>
     ),
     
-    // Weapons icon - crossed swords
+    // Weapons icon - sword
     weapons: (
       <svg {...iconProps}>
-        <path d="M6.92 5H5L14 14L15 13L6.92 5Z" />
-        <path d="M22.54 6.25L21.15 4.86L19.75 6.25L21.15 7.64L22.54 6.25Z" />
-        <path d="M14.96 11.5L13.57 10.11L12.18 11.5L13.57 12.89L14.96 11.5Z" />
-        <path d="M20.29 15.46L19.5 16.25L17.5 14.25L18.29 13.46L20.29 15.46Z" />
-        <path d="M5.03 18.95L6.42 20.34L7.81 18.95L6.42 17.56L5.03 18.95Z" />
-        <path d="M13.95 3.79L12.56 2.4L11.17 3.79L12.56 5.18L13.95 3.79Z" />
+        <path d="M6 2L8 4L6 6L4 4L6 2Z" />
+        <path d="M6 6L6 18L8 20L8 6L6 6Z" />
+        <path d="M4 18L8 18L8 20L4 20L4 18Z" />
+        <path d="M5 19L7 19L7 21L5 21L5 19Z" />
+        <path d="M6 2L6 4L8 4L8 2L6 2Z" />
+        <path d="M2 4L4 6L6 4L4 2L2 4Z" />
+        <path d="M8 4L10 6L12 4L10 2L8 4Z" />
       </svg>
     ),
     
@@ -50,17 +51,27 @@ export const Icon = ({ name, size = 20, color = '#8b949e', className = '' }) => 
       </svg>
     ),
     
-    // Army Books icon - book
+    // Army Books icon - detailed book with pages
     armybooks: (
       <svg {...iconProps}>
-        <path d="M4 6C4 4.9 4.9 4 6 4H18C19.1 4 20 4.9 20 6V18C20 19.1 19.1 20 18 20H6C4.9 20 4 19.1 4 18V6Z" />
-        <path d="M8 8H16V10H8V8Z" />
-        <path d="M8 12H16V14H8V12Z" />
-        <path d="M8 16H14V18H8V16Z" />
+        <path d="M4 4C4 2.9 4.9 2 6 2H18C19.1 2 20 2.9 20 4V20C20 21.1 19.1 22 18 22H6C4.9 22 4 21.1 4 20V4Z" />
+        <path d="M6 4H18V6H6V4Z" />
+        <path d="M6 6H18V8H6V6Z" />
+        <path d="M6 8H18V10H6V8Z" />
+        <path d="M6 10H18V12H6V10Z" />
+        <path d="M6 12H18V14H6V12Z" />
+        <path d="M6 14H18V16H6V14Z" />
+        <path d="M6 16H18V18H6V16Z" />
+        <path d="M6 18H18V20H6V18Z" />
+        <path d="M8 4V20" />
+        <path d="M10 4V20" />
+        <path d="M12 4V20" />
+        <path d="M14 4V20" />
+        <path d="M16 4V20" />
       </svg>
     ),
     
-    // Army Lists icon - clipboard/list
+    // Army Lists icon - detailed clipboard with checkboxes
     armylists: (
       <svg {...iconProps}>
         <path d="M9 2H15C16.1 2 17 2.9 17 4V20C17 21.1 16.1 22 15 22H9C7.9 22 7 21.1 7 20V4C7 2.9 7.9 2 9 2Z" />
@@ -69,16 +80,42 @@ export const Icon = ({ name, size = 20, color = '#8b949e', className = '' }) => 
         <path d="M11 10H13V12H11V10Z" />
         <path d="M11 14H13V16H11V14Z" />
         <path d="M11 18H13V20H11V18Z" />
+        <path d="M8 2H10V4H8V2Z" />
+        <path d="M8 4H10V6H8V4Z" />
+        <path d="M8 6H10V8H8V6Z" />
+        <path d="M8 8H10V10H8V8Z" />
+        <path d="M8 10H10V12H8V10Z" />
+        <path d="M8 12H10V14H8V12Z" />
+        <path d="M8 14H10V16H8V14Z" />
+        <path d="M8 16H10V18H8V16Z" />
+        <path d="M8 18H10V20H8V18Z" />
+        <path d="M8 20H10V22H8V20Z" />
       </svg>
     ),
     
-    // Import icon - upload
+    // Import icon - detailed upload with files
     import: (
       <svg {...iconProps}>
         <path d="M14 2H6C4.9 2 4 2.9 4 4V20C4 21.1 4.9 22 6 22H18C19.1 22 20 21.1 20 20V8L14 2Z" />
         <path d="M14 2V8H20" />
         <path d="M16 13H8" />
         <path d="M12 9L16 13L12 17" />
+        <path d="M6 4H8V6H6V4Z" />
+        <path d="M6 6H8V8H6V6Z" />
+        <path d="M6 8H8V10H6V8Z" />
+        <path d="M6 10H8V12H6V10Z" />
+        <path d="M6 12H8V14H6V12Z" />
+        <path d="M6 14H8V16H6V14Z" />
+        <path d="M6 16H8V18H6V16Z" />
+        <path d="M6 18H8V20H6V18Z" />
+        <path d="M10 4H12V6H10V4Z" />
+        <path d="M10 6H12V8H10V6Z" />
+        <path d="M10 8H12V10H10V8Z" />
+        <path d="M10 10H12V12H10V10Z" />
+        <path d="M10 12H12V14H10V12Z" />
+        <path d="M10 14H12V16H10V14Z" />
+        <path d="M10 16H12V18H10V16Z" />
+        <path d="M10 18H12V20H10V18Z" />
       </svg>
     ),
     
@@ -151,12 +188,16 @@ export const Icon = ({ name, size = 20, color = '#8b949e', className = '' }) => 
       </svg>
     ),
     
-    // Factions icon - shield with star
+    // Factions icon - detailed shield with banner
     factions: (
       <svg {...iconProps}>
+        <path d="M12 1L3 5V11C3 16.55 6.84 21.74 12 23C17.16 21.74 21 16.55 21 11V5L12 1Z" />
+        <path d="M12 6L8 8V12C8 14.5 9.5 16.5 12 17C14.5 16.5 16 14.5 16 12V8L12 6Z" />
         <path d="M12 2L13.09 8.26L20 9L13.09 9.74L12 16L10.91 9.74L4 9L10.91 8.26L12 2Z" />
-        <path d="M12 2L3 6L12 10L21 6L12 2Z" />
-        <path d="M12 10V22" />
+        <path d="M12 8L10 9L12 10L14 9L12 8Z" />
+        <path d="M12 10L10 11L12 12L14 11L12 10Z" />
+        <path d="M12 12L10 13L12 14L14 13L12 12Z" />
+        <path d="M12 14L10 15L12 16L14 15L12 14Z" />
       </svg>
     )
   };
